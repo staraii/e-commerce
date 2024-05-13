895990e66075c6c479f3c5d56332a0722b9967eb
Staffan Björk <staraii102@gmail.com>
2024-05-13 08:55:00 +0200

    firebase config

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 	apiKey: import.meta.env.DB_PASSWORD,
 	authDomain: import.meta.env.AUTH_DOMAIN,
-	projectId: "",
-	storageBucket: "",
-	messagingSenderId: "",
-	appId: "",
-	measurementId: "",
+	projectId: import.meta.env.PROJECT_ID,
+	storageBucket: import.meta.env.STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+	appId: import.meta.env.APP_ID,
+	measurementId: import.meta.env.MEASUREMENT_ID,
 };
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<App />
