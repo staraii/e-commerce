6c33208753eff42294fb6fc9d00f30c44b764311
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-05-13 08:59:49 +0200

    wrote a comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import ReduxTest from "components/ReduxTest";
-import './App.css'
-
+import "./App.css";
 
 function App() {
-
-	return (
-		<section>
-			<ReduxTest />
-		</section>
-	)
+  return (
+    <section>
+      <ReduxTest />
+    </section>
+  );
 }
 
-export default App
+export default App;
+
+// I think this is a good start <3
