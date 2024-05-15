ff0896a4b9c300d63a68e2d1df21f51562e5e2dc
Johan Håkansson <johan1hakansson@gmail.com>
2024-05-15 12:05:49 +0200

    fixat homepageui, väldigt basic utan funktion och har inte tänkt något på responsiv

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,10 +3,33 @@ import { Link } from 'react-router-dom';
 function Home() {
 	return (
 		<section className={styles.section}>
-			<h2>Home</h2>
-			<p>test</p>
-			<Link to='/products'>Products</Link>
-			<Link to='/cart'>Cart</Link>
+			<nav className={styles.navBar}>
+				<Link to='/products'>Products</Link>
+				<Link to='/cart'>Cart</Link>
+			</nav>
+
+			<div className={styles.container}>
+				<section className={styles.productSection}>
+					<h2>Köp något för att bli glad</h2>
+					<h4>Men köp något då</h4>
+					<section className={styles.productPics}>
+						<div className={styles.product}>bild här</div>
+						<div className={styles.product}>bild här</div>
+						<div className={styles.product}>bild här</div>
+						<div className={styles.product}>bild här</div>
+						<div className={styles.product}>bild här</div>
+					</section>
+				</section>
+
+				<section className={styles.categorySection}>
+					<h2>Katergori</h2>
+					<button className={styles.btn}>Byxor</button>
+					<button className={styles.btn}>Byxor</button>
+					<button className={styles.btn}>Byxor</button>
+					<button className={styles.btn}>Byxor</button>
+					<button className={styles.btn}>Byxor</button>
+				</section>
+			</div>
 		</section>
 	);
 }
