0e6e6a9302a791952b6eb88b7f54b51dfa2fa072
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-05-15 15:15:40 +0200

    cart_uppdate_functionality from cartSlice and modify Cart with that

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,39 +1,58 @@
 import React from "react";
-import styles from "./cart.module.css";
+import { useSelector, useDispatch } from "react-redux";
 
+import {
+  removeItem,
+  incrementItem,
+  decrementItem,
+} from "../../slices/cartSlice";
+import styles from "./cart.module.css";
 import { Link } from "react-router-dom";
 
+interface CartItem {
+  productName: string;
+  productId: ProductId;
+  price: number;
+  quantity: number;
+}
+
+type ProductId = string;
+
 const Cart: React.FC = () => {
+  const cartItems = useSelector((state: any) => state.cart.items);
+  const dispatch = useDispatch();
+
+  const handleIncrement = (productId: ProductId) => {
+    dispatch(incrementItem(productId));
+  };
+
+  const handleDecrement = (productId: ProductId) => {
+    dispatch(decrementItem(productId));
+  };
+
+  const handleRemove = (productId: ProductId) => {
+    dispatch(removeItem(productId));
+  };
+
   return (
     <>
       <div className={styles.bg_Welcome}>
         <h1 className={styles.H_One}>Welcome to Cart!</h1>
+        <p>
+          <p className={styles.cartQuantity}>{cartItems.length} pcs</p>
+        </p>
       </div>{" "}
       <section className={styles.section}>
-        <div className={styles.item}>
-          {<h3>En Product</h3>}
-          <button className={styles.deleteItem}>Remove</button>
-        </div>
-        <div className={styles.item}>
-          {<h3>En Product</h3>}
-          <button className={styles.deleteItem}>Remove</button>
-        </div>
-        <div className={styles.item}>
-          {<h3>En Product</h3>}
-          <button className={styles.deleteItem}>Remove</button>
-        </div>
-        <div className={styles.item}>
-          {<h3>En Product</h3>}
-          <button className={styles.deleteItem}>Remove</button>
-        </div>
-        <div className={styles.item}>
-          {<h3>En Product</h3>}
-          <button className={styles.deleteItem}>Remove</button>
-        </div>
-        <div className={styles.item}>
-          {<h3>En Product</h3>}
-          <button className={styles.deleteItem}>Remove</button>
-        </div>
+        {cartItems.map((item: CartItem) => (
+          <div className={styles.item} key={item.productId}>
+            <h3>{item.productName}</h3>
+            <p>Price: ${item.price}</p>
+            <p>Quantity: {item.quantity}</p>
+            <button onClick={() => handleIncrement(item.productId)}>+</button>
+            <button onClick={() => handleDecrement(item.productId)}>-</button>
+            <button onClick={() => handleRemove(item.productId)}>Remove</button>
+          </div>
+        ))}
       </section>
       <section className={styles.purchaseSection}>
         <p className={styles.paragraph}>
