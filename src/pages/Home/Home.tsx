fd9fe1b85abfd973e9900971f19d913a0c005e02
Johan Håkansson <johan1hakansson@gmail.com>
2024-05-15 10:32:26 +0200

    test commit

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,14 @@
-import styles from "./home.module.css";
-import { Link } from "react-router-dom";
+import styles from './home.module.css';
+import { Link } from 'react-router-dom';
 function Home() {
 	return (
 		<section className={styles.section}>
 			<h2>Home</h2>
-			<Link to="/products">Products</Link>
-			<Link to="/cart">Cart</Link>
+			<p>test</p>
+			<Link to='/products'>Products</Link>
+			<Link to='/cart'>Cart</Link>
 		</section>
-	)
+	);
 }
 
-export default Home
+export default Home;
