8a01b73e0eb9de0d63d7fd1a3b3d4352532fa296
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-05-15 13:57:59 +0200

    I have fixed the basic styling and changed a little in the root boxshadow and other_cart_color

--- src/pages/Cart.tsx
+++
@@ -1,9 +0,0 @@
-function Cart() {
-  return (
-    <section>
-      <h2>Cart</h2>
-    </section>
-  );
-}
-
-export default Cart;
--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,6 +1,51 @@
+import React from "react";
 import styles from "./cart.module.css";
-function Cart() {
-	return <section className={styles.section}>Cart</section>;
-}
+
+import { Link } from "react-router-dom";
+
+const Cart: React.FC = () => {
+  return (
+    <>
+      <div className={styles.bg_Welcome}>
+        <h1 className={styles.H_One}>Welcome to Cart!</h1>
+      </div>{" "}
+      <section className={styles.section}>
+        <div className={styles.item}>
+          {<h3>En Product</h3>}
+          <button className={styles.deleteItem}>Remove</button>
+        </div>
+        <div className={styles.item}>
+          {<h3>En Product</h3>}
+          <button className={styles.deleteItem}>Remove</button>
+        </div>
+        <div className={styles.item}>
+          {<h3>En Product</h3>}
+          <button className={styles.deleteItem}>Remove</button>
+        </div>
+        <div className={styles.item}>
+          {<h3>En Product</h3>}
+          <button className={styles.deleteItem}>Remove</button>
+        </div>
+        <div className={styles.item}>
+          {<h3>En Product</h3>}
+          <button className={styles.deleteItem}>Remove</button>
+        </div>
+        <div className={styles.item}>
+          {<h3>En Product</h3>}
+          <button className={styles.deleteItem}>Remove</button>
+        </div>
+      </section>
+      <section className={styles.purchaseSection}>
+        <p className={styles.paragraph}>
+          Click the button below to proceed to checkout.
+        </p>
+        <button className={styles.CheckoutBtn}>Checkout</button>
+      </section>
+      <section className={styles.backSection}>
+        <Link to="/">Back to Homepage</Link>
+      </section>
+    </>
+  );
+};
 
 export default Cart;
