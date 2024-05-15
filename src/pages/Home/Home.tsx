1393af7a90c52cd6197112737bf5951caab3ddf9
Johan Håkansson <johan1hakansson@gmail.com>
2024-05-15 14:03:17 +0200

    Homepage kommer se ut typ så här tänker jag mig

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,8 +4,12 @@ function Home() {
 	return (
 		<section className={styles.section}>
 			<nav className={styles.navBar}>
-				<Link to='/products'>Products</Link>
-				<Link to='/cart'>Cart</Link>
+				<Link className={styles.link} to='/products'>
+					Products
+				</Link>
+				<Link className={styles.link} to='/cart'>
+					Cart
+				</Link>
 			</nav>
 
 			<div className={styles.container}>
@@ -13,21 +17,38 @@ function Home() {
 					<h2>Köp något för att bli glad</h2>
 					<h4>Men köp något då</h4>
 					<section className={styles.productPics}>
-						<div className={styles.product}>bild här</div>
-						<div className={styles.product}>bild här</div>
-						<div className={styles.product}>bild här</div>
-						<div className={styles.product}>bild här</div>
-						<div className={styles.product}>bild här</div>
+						<Link className={styles.product} to='/cart'>
+							Bild på produkt
+						</Link>
+						<Link className={styles.product} to='/cart'>
+							Bild på produkt
+						</Link>
+						<Link className={styles.product} to='/cart'>
+							Bild på produkt
+						</Link>
+						<Link className={styles.product} to='/cart'>
+							Bild på produkt
+						</Link>
+						<Link className={styles.product} to='/cart'>
+							Bild på produkt
+						</Link>
 					</section>
 				</section>
 
 				<section className={styles.categorySection}>
 					<h2>Katergori</h2>
-					<button className={styles.btn}>Byxor</button>
-					<button className={styles.btn}>Byxor</button>
-					<button className={styles.btn}>Byxor</button>
-					<button className={styles.btn}>Byxor</button>
-					<button className={styles.btn}>Byxor</button>
+					<Link className={styles.categoryLink} to='/cart'>
+						Skor
+					</Link>
+					<Link className={styles.categoryLink} to='/cart'>
+						Tröjor
+					</Link>
+					<Link className={styles.categoryLink} to='/cart'>
+						Byxor
+					</Link>
+					<Link className={styles.categoryLink} to='/cart'>
+						Hattar
+					</Link>
 				</section>
 			</div>
 		</section>
