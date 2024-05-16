b3bec0c4f61ece08b298ae8cc6ae60590b863f6c
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-05-16 11:32:19 +0200

    fixed bugg att app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ReduxTest from "components/ReduxTest/ReduxTest";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "pages/Home/Home";
@@ -23,32 +22,6 @@ function App() {
       <Fotter />
     </section>
   );
-=======
-import ReduxTest from 'components/ReduxTest/ReduxTest';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from 'pages/Home/Home';
-import Products from 'pages/Products/Products';
-import Cart from 'pages/Cart/Cart';
-import Admin from 'pages/Admin/Admin';
-import NavBar from 'components/NavBar/NavBar';
-import './App.css';
-
-function App() {
-	return (
-		<section>
-			<Router>
-				<NavBar />
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/products' element={<Products />} />
-					<Route path='/cart' element={<Cart />} />
-					<Route path='/admin' element={<Admin />} />
-					<Route path='/redux' element={<ReduxTest />} />
-				</Routes>
-			</Router>
-		</section>
-	);
->>>>>>> d27f6453c14652838a384e43c53e6ba61e80e2b5
 }
 
 export default App;
