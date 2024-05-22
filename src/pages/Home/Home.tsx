2c9a2a2e00bf6624e3fb2dbcc7acea98c0895753
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-05-22 11:41:50 +0200

    try to make home responsiv

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,64 +1,48 @@
-import styles from './home.module.css';
-import { Link } from 'react-router-dom';
-import { TypeAnimation } from 'react-type-animation';
-import { useEffect } from 'react';
+import styles from "./home.module.css";
+import { Link } from "react-router-dom";
+import { TypeAnimation } from "react-type-animation";
+import { useEffect } from "react";
 import GetProducts, {
-	Product,
-} from 'components/HomePageProducts/HomePageProducts';
+  Product,
+} from "components/HomePageProducts/HomePageProducts";
 
 function Home() {
-	useEffect(() => {
-		// Funktion för att hämta produkter
-		const fetchData = async () => {
-			await GetProducts(3);
-		};
+  useEffect(() => {
+    // Funktion för att hämta produkter
+    const fetchData = async () => {
+      await GetProducts(3);
+    };
 
-		// Anropa funktionen
-		fetchData();
-	}, []);
+    // Anropa funktionen
+    fetchData();
+  }, []);
 
-	return (
-		<section className={styles.section}>
-			<div className={styles.container}>
-				<section className={styles.productSection}>
-					<TypeAnimation
-						sequence={[
-							'Why dont you checkout our Jackets?',
-							2000,
-							'Why dont you checkout our Shoes?',
-							2000,
-							'Why dont you checkout our Pants?',
-							2000,
-							'Why dont you checkout our Shirts?',
-							2000,
-						]}
-						speed={40}
-						style={{ fontSize: '3rem', fontWeight: 'bold' }}
-						repeat={Infinity}
-					/>
-					<section className={styles.productPics}>
-						<Product />
-					</section>
-				</section>
-
-				<aside className={styles.asideSection}>
-					<h2 className={styles.categorySectionH2}>Categories</h2>
-					<Link className={styles.asideLink} to='/cart'>
-						Shoes
-					</Link>
-					<Link className={styles.asideLink} to='/cart'>
-						Shirts
-					</Link>
-					<Link className={styles.asideLink} to='/cart'>
-						Pants
-					</Link>
-					<Link className={styles.asideLink} to='/cart'>
-						Head wear
-					</Link>
-				</aside>
-			</div>
-		</section>
-	);
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <section className={styles.productSection}>
+          <TypeAnimation
+            sequence={[
+              "Why dont you checkout our Jackets?",
+              2000,
+              "Why dont you checkout our Shoes?",
+              2000,
+              "Why dont you checkout our Pants?",
+              2000,
+              "Why dont you checkout our Shirts?",
+              2000,
+            ]}
+            speed={40}
+            style={{ fontSize: "3rem", fontWeight: "bold" }}
+            repeat={Infinity}
+          />
+          <section className={styles.productPics}>
+            <Product />
+          </section>
+        </section>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
